c42033835648ca545e4211a5b3c7ccfd062d62e8
Sean Fang <fangzhengonly@gmail.com>
2023-08-29 15:45:15 -0400

    draw scatter plot

---
+++ section-4-1-STARTER/app.js
@@ -0,0 +1,98 @@
+async function draw() {
+  // grap data
+  const dataset = await d3.json('data.json');
+
+  const xAccessor = (d) => d.currently.humidity;
+  const yAccessor = (d) => d.currently.apparentTemperature;
+
+  // dimensions
+  let dimensions = {
+    width: 800,
+    height: 800,
+    margin: {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50,
+    },
+  };
+
+  dimensions.ctrWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.ctrHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // draw image
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  const ctr = svg
+    .append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.margin.left},${dimensions.margin.top})`
+    );
+
+  // scales
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .rangeRound([0, dimensions.ctrWidth])
+    .clamp(true);
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .rangeRound([dimensions.ctrHeight, 0])
+    .nice()
+    .clamp(true);
+
+  // draw circles
+  ctr
+    .selectAll('circle')
+    .data(dataset)
+    .join('circle')
+    .attr('cx', (d) => xScale(xAccessor(d)))
+    .attr('cy', (d) => yScale(yAccessor(d)))
+    .attr('r', 5)
+    .attr('fill', 'red')
+    .attr('date-temp', yAccessor);
+
+  // axes
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(5)
+    // .tickValues([0.4,0.5,0.8])
+    .tickFormat((d) => {
+      return d * 100 + '%';
+    });
+
+  const xAxisGroup = ctr
+    .append('g')
+    .call(xAxis)
+    .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+    .classed('axis', true);
+
+  xAxisGroup
+    .append('text')
+    .attr('x', dimensions.ctrWidth / 2)
+    .attr('y', dimensions.margin.bottom - 10)
+    .attr('fill', 'black')
+    .text('humidity');
+
+  const yAxis = d3.axisLeft(yScale);
+  const yAxisGroup = ctr.append('g').call(yAxis).classed('axis', true);
+
+  yAxisGroup
+    .append('text')
+    .attr('x', -dimensions.ctrHeight / 2)
+    .attr('y', -dimensions.margin.left + 15)
+    .attr('fill', 'black')
+    .html('Temperature &deg; F')
+    .style('transform', 'rotate(270deg)')
+    .style('text-anchor', 'middle');
+}
+
+draw();
