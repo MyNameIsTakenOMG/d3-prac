4bfdeb77bdb754795fb8a51d389eeffd13a616d2
Sean Fang <fangzhengonly@gmail.com>
2023-08-29 12:46:22 -0400

    csv request

--- app.js
+++ app.js
@@ -46,7 +46,8 @@
 // console.log(el);
 
 async function getData() {
-  const data = await d3.json('data.json');
+  //const data = await d3.json('data.json');
+  const data = await d3.csv('data.csv');
   console.log(data);
 }
 getData();
